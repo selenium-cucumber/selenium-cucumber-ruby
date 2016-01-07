310271a6d8e335e6eb5c2a536c7904abc2e5fd6b
Alexey Lustin <allustin@users.noreply.github.com>
2016-01-07 23:08:30 +0300

    quit the driver at cucumber exit
    * Add 2 standart cucumber hooks
    * close the driver process after the test sesion is ended

--- features-skeleton/support/hooks.rb
+++ features-skeleton/support/hooks.rb
@@ -1,4 +1,7 @@
 #Cucumber provides a number of hooks which allow us to run blocks at various points in the Cucumber test cycle
+AfterConfiguration do |config|
+  # do something before Cucumber run test cycle and after it already configure 
+end
 
 Before do
   # Do something before each scenario.
@@ -35,4 +38,9 @@ end
 AfterStep('@Ex_tag1, @Ex_tag2') do
   # This will only run after steps within scenarios tagged
   # with @cucumis AND @sativus.
+end
+
+at_exit do
+  # Do something in the end of cucumber test cycle
+  $driver.quit # process for driver will be close with all windows 
 end
