77c9017e091866a934ba12b7ccf36426261f6463
sameer49 <sameersawant222@gmail.com>
2017-04-24 20:55:30 +0530

    adding support for saucelab and browserstack platforms

--- features-skeleton/support/env.rb
+++ features-skeleton/support/env.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'selenium-cucumber'
+require 'yaml'
 
 # Store command line arguments
 $browser_type = ENV['BROWSER'] || 'ff'
@@ -8,44 +9,46 @@ $os_version = ENV['OS_VERSION']
 $device_name = ENV['DEVICE_NAME']
 $udid = ENV['UDID']
 $app_path = ENV['APP_PATH']
+$config_file = ENV['PLATFORM_CONFIG']
 
-# check for valid parameters
-validate_parameters $platform, $browser_type, $app_path
+if $config_file
+  $driver = get_cloud_platform_driver($config_file, __dir__)
+else
+  # If platform is android or ios create driver instance for mobile browser
+  if $platform == 'android' or $platform == 'iOS'
+    
+    if $browser_type == 'native'
+      $browser_type = "Browser"
+    end
+    
+    if $platform == 'android'
+      $device_name, $os_version = get_device_info
+    end
+    
+    desired_caps = {
+      caps:       {
+        platformName:  $platform,
+        browserName: $browser_type,
+        versionNumber: $os_version,
+        deviceName: $device_name,
+        udid: $udid,
+        app: ".//#{$app_path}"
+        },
+      }
 
-# If platform is android or ios create driver instance for mobile browser
-if $platform == 'android' or $platform == 'iOS'
-  
-  if $browser_type == 'native'
-    $browser_type = "Browser"
-  end
-  
-  if $platform == 'android'
-    $device_name, $os_version = get_device_info
-  end
-  
-  desired_caps = {
-    caps:       {
-      platformName:  $platform,
-      browserName: $browser_type,
-      versionNumber: $os_version,
-      deviceName: $device_name,
-      udid: $udid,
-      app: ".//#{$app_path}"
-      },
-    }
-
-  begin
-    $driver = Appium::Driver.new(desired_caps).start_driver
-  rescue Exception => e
-    puts e.message
-    Process.exit(0)  
-  end
-else # else create driver instance for desktop browser
-  begin
-    $driver = Selenium::WebDriver.for(:"#{$browser_type}")
-    $driver.manage().window().maximize()
-  rescue Exception => e
-    puts e.message
-    Process.exit(0)
+    begin
+      $driver = Appium::Driver.new(desired_caps).start_driver
+    rescue Exception => e
+      puts e.message
+      Process.exit(0)  
+    end
+  else # else create driver instance for desktop browser
+    begin
+      $driver = Selenium::WebDriver.for(:"#{$browser_type}")
+      $driver.manage().window().maximize()
+    rescue Exception => e
+      puts e.message
+      Process.exit(0)
+    end
   end
 end
