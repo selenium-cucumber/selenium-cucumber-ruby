9cba30b7a017907384861ffffcc54b55780a77d3
sameer49 <sameersawant222@gmail.com>
2017-07-10 20:18:13 +0530

    added upload app function call

--- features-skeleton/support/env.rb
+++ features-skeleton/support/env.rb
@@ -3,9 +3,15 @@ require 'selenium-cucumber'
 require 'yaml'
 
 # Store command line arguments
+$upload_app = ENV['UPLOAD_APP']
 $platform = ENV['PLATFORM'] || 'desktop'
 $browser_type = ENV['BROWSER'] || 'ff'
 $config_file = ENV['PLATFORM_CONFIG']
+
+if $upload_app
+	upload_app_to_cloud_platform($platform, $upload_app, __dir__)
+	Process.exit(0)
+end
 
 validate_parameters $platform, $browser_type, $app_path, "4.0.0"
 
--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -3,11 +3,17 @@ require 'selenium-cucumber'
 require 'yaml'
 
 # Store command line arguments
+$upload_app = ENV['UPLOAD_APP']
 $platform = ENV['PLATFORM'] || 'desktop'
 $browser_type = ENV['BROWSER'] || 'ff'
 $config_file = ENV['PLATFORM_CONFIG']
 
-validate_parameters $platform, $browser_type, $app_path
+if $upload_app
+	upload_app_to_cloud_platform($platform, $upload_app, __dir__)
+	Process.exit(0)
+end
+
+validate_parameters $platform, $browser_type, $app_path, "4.0.0"
 
 if $config_file
   $driver = get_platform_driver($config_file, __dir__)
@@ -21,3 +27,5 @@ else
     Process.exit(0)
   end
 end
+
+$driver.manage.timeouts.implicit_wait = 5
