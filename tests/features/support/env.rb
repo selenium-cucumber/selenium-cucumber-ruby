e343cecc78f722f94e9e1893f7d27271bac1c897
sameer49 <sameersawant222@gmail.com>
2017-08-17 20:57:56 +0530

    changed vaible name

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -1,25 +1,28 @@
 require 'rubygems'
 require 'selenium-cucumber'
 require 'yaml'
+require 'openssl'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+        
 
 # Store command line arguments
 $upload_app = ENV['UPLOAD_APP']
 $platform = ENV['PLATFORM'] || 'desktop'
-$browser_type = ENV['BROWSER'] || 'ff'
+$browser = ENV['BROWSER'] || 'ff'
 $config_file = ENV['PLATFORM_CONFIG']
-
 if $upload_app
-	upload_app_to_cloud_platform($platform, $upload_app, __dir__)
+  upload_app_to_cloud_platform($platform, $upload_app, __dir__)
 	Process.exit(0)
 end
 
-validate_parameters $platform, $browser_type, $app_path, "4.0.0"
+validate_parameters $platform, $browser, $app_path, "4.0.0"
 
 if $config_file
   $driver = get_platform_driver($config_file, __dir__)
 else
   begin
-    $driver = Selenium::WebDriver.for(:"#{$browser_type}")
+    $driver = Selenium::WebDriver.for(:"#{$browser}")
     $driver.manage().window().maximize()
   rescue Exception => e
     puts e.message
