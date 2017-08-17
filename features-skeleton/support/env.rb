becf2531f33695ebeffe61b365530e792d17ba23
sameer49 <sameersawant222@gmail.com>
2017-08-17 20:47:16 +0530

    changed variable name to browser from browser_type

--- features-skeleton/support/env.rb
+++ features-skeleton/support/env.rb
@@ -5,21 +5,20 @@ require 'yaml'
 # Store command line arguments
 $upload_app = ENV['UPLOAD_APP']
 $platform = ENV['PLATFORM'] || 'desktop'
-$browser_type = ENV['BROWSER'] || 'ff'
+$browser = ENV['BROWSER'] || 'ff'
 $config_file = ENV['PLATFORM_CONFIG']
-
 if $upload_app
-	upload_app_to_cloud_platform($platform, $upload_app, __dir__)
-	Process.exit(0)
+  upload_app_to_cloud_platform($platform, $upload_app, __dir__)
+  Process.exit(0)
 end
 
-validate_parameters $platform, $browser_type, $app_path, "4.0.0"
+validate_parameters $platform, $browser, $app_path, "4.0.0"
 
 if $config_file
   $driver = get_platform_driver($config_file, __dir__)
 else
   begin
-    $driver = Selenium::WebDriver.for(:"#{$browser_type}")
+    $driver = Selenium::WebDriver.for(:"#{$browser}")
     $driver.manage().window().maximize()
   rescue Exception => e
     puts e.message
